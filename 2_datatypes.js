08b0435e3538c977e98240708da4b06eebf98c78
payal <payalkarn2410@gmail.com>
2026-03-18 17:58:59 +0530

    datatypes in js

---
+++ 2_datatypes.js
@@ -0,0 +1,20 @@
+var number = 5
+console.log(number)
+console.log(typeof number)
+
+var isRainingToday = false
+console.log(isRainingToday)
+console.log(typeof isRainingToday)
+
+var payal;
+console.log(payal)
+console.log(typeof payal) //type of undefined type is undefined
+
+var badMemories = null
+console.log(badMemories)
+console.log(typeof badMemories) //type of null is object
+
+const bigNumber = 1234567890123456789012345678901234567890n
+
+console.log(bigNumber)
+console.log(typeof bigNumber)
